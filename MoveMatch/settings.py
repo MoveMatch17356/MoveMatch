e9c964a83bc961e9c0d5b188c51ef594d8364a85
EyobD <edagnach@andrew.cmu.edu>
2025-05-05 11:44:05 -0400

    Restructure templates to fitApp/templates/fitApp, cleanup old media and samples, update models and views

--- MoveMatch/settings.py
+++ MoveMatch/settings.py
@@ -113,7 +113,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-TIME_ZONE = 'America/New_York'
+    TIME_ZONE = 'America/New_York'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
